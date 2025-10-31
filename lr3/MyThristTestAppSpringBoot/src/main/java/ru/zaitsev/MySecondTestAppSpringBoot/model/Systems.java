7faf6856fff7fe3dd109d137fe3448a7390c80c5
Kirill Zaitsev <Zaitsev.Kirill@urfu.me>
2025-11-01 00:30:52 +0500

    Delete lr3/MyThristTestAppSpringBoot directory

--- lr3/MyThristTestAppSpringBoot/src/main/java/ru/zaitsev/MySecondTestAppSpringBoot/model/Systems.java
+++
@@ -1,7 +0,0 @@
-package ru.zaitsev.MySecondTestAppSpringBoot.model;
-
-public enum Systems {
-    ERP,   // Enterprise Resource Planning
-    CRM,   // Customer Relationship Management
-    WMS    // Warehouse Management System
-}
